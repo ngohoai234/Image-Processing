a07f495fe3179bd88c6c133b2b5727ab3f26af5c
Can Nguyen <cangod05@gmail.com>
2024-10-13 21:04:10 +0700

    add file uti file and format

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,2 +1,32 @@
 import path from 'path';
 import { promises as fs } from 'fs';
+
+import { ImageData } from '../models/File.model';
+import { isStringEmpty } from './format';
+
+class FileUtils {
+  static basePathImage = path.resolve(__dirname, '../assets/images/base ');
+  static resizePathImage = path.resolve(__dirname, '../assets/images/resize ');
+
+  static async getFullPathImage(data: ImageData) {
+    const { name, width, height } = data;
+    if (isStringEmpty(name)) {
+      return null;
+    }
+
+    const filePath =
+      width && height
+        ? path.resolve(
+            FileUtils.resizePathImage,
+            `${name}-${width}x${height}.jpg`
+          )
+        : path.resolve(FileUtils.basePathImage, `${name}.jpg`);
+
+    return await fs
+      .access(filePath)
+      .then(() => filePath)
+      .catch(() => null);
+  }
+}
+
+export default FileUtils;
